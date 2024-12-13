61f8bfda4a048f1aeb41cbf2bb24810a6a4694b6
Juan Arroyave <juan@scalista.com>
2024-12-13 09:06:43 +0000

    chore: automated commit before release

--- scripts/pre-release-commands.sh
+++ scripts/pre-release-commands.sh
@@ -2,17 +2,18 @@
 
 echo "Running pre-release commands" 
 
-echo "Checking if there are any changes in the current branch"
+echo "Checking if there are uncommitted changes"
 
 if ! git diff --exit-code; then
-  echo "There are changes in the current branch"
-  exit 1
-fi
-
-echo "Checking if there are any changes in the main branch"
-if ! git diff --exit-code origin/main; then
-  echo "There are changes in the main branch"
-  exit 1
+  echo "There are uncommitted changes"
+  echo "Do you want to add and commit automatically? (y/n)"
+  read -r answer
+  if [ "$answer" = "y" ]; then
+    git add .
+    git commit -m "chore: automated commit before release"
+  else
+    exit 1
+  fi
 fi
 
 npx nx release plan --base=origin/main --head=HEAD --only-touched=false
