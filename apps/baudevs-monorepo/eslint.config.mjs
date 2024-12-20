6a4129ae5094de255f7628515ad437ee4a853a41
Juan Arroyave <juan@scalista.com>
2024-12-20 12:57:18 +0100

    breaking: migrate project to Bun, update dependencies, and add workspaces
    - Switched package manager from npm to Bun for improved performance.
    - Updated dependencies in package.json, including Next.js and React versions.
    - Added workspaces configuration for better local development of new BauCMS libraries.
    - Introduced ESLint configuration files for various applications and libraries.
    - Updated .gitignore to exclude new code snippets and files.
    - Removed outdated package-lock.json files and added new bun.lockb.
    - Enhanced nx.json with new build configurations and cache settings.
    - Created version plan for BauLogHero indicating major changes.

--- apps/baudevs-monorepo/eslint.config.mjs
+++ apps/baudevs-monorepo/eslint.config.mjs
@@ -1,15 +1,15 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const js = require('@eslint/js');
-const { fixupConfigRules } = require('@eslint/compat');
-const nx = require('@nx/eslint-plugin');
-const baseConfig = require('../../eslint.config.js');
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { fixupConfigRules } from '@eslint/compat';
+import nx from '@nx/eslint-plugin';
+import baseConfig from '../../eslint.config.mjs';
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
 });
 
-module.exports = [
+export default [
   ...fixupConfigRules(compat.extends('next')),
 
   ...fixupConfigRules(compat.extends('next/core-web-vitals')),
