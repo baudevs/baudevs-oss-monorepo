0f8e8e314ecba7c3e81b58148fc0ee605f34e2c3
Juan Arroyave <juan@scalista.com>
2024-12-22 19:22:53 +0100

    feat: update App component to use MUI for enhanced UI
    - Refactored the App component to utilize Material-UI (MUI) for improved styling and layout.
    - Replaced the previous NxWelcome component with a Box containing a Typography header and a Button.
    - Updated the layout to center the content vertically and horizontally with a gray background.
    - This change enhances the user interface and provides a more modern look for the baudevs-dashboard application.

--- apps/baudevs-dashboard/src/app/app.tsx
+++ apps/baudevs-dashboard/src/app/app.tsx
@@ -1,13 +1,18 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { Box, Button, Typography } from '@mui/material';
 
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="baudevs-dashboard" />
-    </div>
-  );
-}
+
+  export function App() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-50">
+        <Box>
+          <Typography variant="h4">Baudevs Dashboard</Typography>
+          <Button variant="contained" color="primary" className="mt-4">
+            Hello from MUI
+          </Button>
+        </Box>
+      </div>
+    );
+  }
+
 
 export default App;
