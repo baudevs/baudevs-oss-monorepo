e141a17f42778d5073a3b70421b81cc65bebf2e1
Juan Arroyave <juan@scalista.com>
2025-01-05 22:12:54 +0100

    chore(release-tools): enhance build configuration and update dependencies
    - Added several Rollup plugins including `@rollup/plugin-commonjs`, `@rollup/plugin-node-resolve`, `@rollup/plugin-terser`, and `rollup-plugin-gzip` to improve the build process in `release-tools`.
    - Updated `pnpm-lock.yaml` and `package.json` to reflect the new dependencies and their versions.
    - Modified the Rollup configuration to utilize the new plugins for better optimization and gzip compression.
    - Updated GitHub workflows to use the latest Node.js and PNPM versions for consistency in CI environments.

--- tools/release-tools/rollup.config.mjs
+++ tools/release-tools/rollup.config.mjs
@@ -1,6 +1,11 @@
 import { withNx } from '@nx/rollup/with-nx.js';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import gzipPlugin from 'rollup-plugin-gzip';
 
-
+// Check if we're in CI environment
+const isCI = process.env.CI === 'true';
 
 export default withNx(
   {
@@ -16,13 +21,31 @@ export default withNx(
     output: {
       name: 'ReleaseTools',
       sourcemap: true,
-      plugins: [],
+      plugins: [terser()],
       globals: {
         '@baudevs/release-tools': 'ReleaseTools',
       },
     },
     external: [],
     plugins: [
+      nodeResolve(),
+      commonjs(),
+      terser({
+        compress: {
+          ecma: 2020,
+          booleans_as_integers: true,
+          ...isCI && {
+            pure_funcs: ['console.log', 'console.debug'],
+            drop_console: ['debug', 'log'],
+          },
+        },
+        format: {
+          comments: false,
+        },
+      }),
+      gzipPlugin({
+        filter: (file) => file.endsWith('.umd.js'),
+      }),
     ],
   },
 );
